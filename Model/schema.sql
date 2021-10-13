6023326fc5c746b9d4b5214b732a63624f476424
timotius anrez <anrezt@gmail.com>
2021-10-13 12:30:43 +0000

    update model scema

--- Model/schema.sql
+++ Model/schema.sql
@@ -1,59 +1,59 @@
-CREATE DATABASE instagram;
-USE instagram
+DROP DATABASE IF EXISTS ig_clone;
+CREATE DATABASE ig_clone;
+USE ig_clone; 
 
-CREATE TABLE Users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    userName VARCHAR(100) UNIQUE NOT NULL,
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE Photos(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     image_url VARCHAR(255) NOT NULL,
-    users_id INT NOT NULL,
+    user_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (users_id) REFERENCES Users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE Comments(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    comment_content VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    photo_id INT NOT NULL,
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    photo_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES Users(id),
-    FOREIGN KEY (photo_id) REFERENCES Photos(id)
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE Likes(
-    photo_id INT NOT NULL,
-    user_id INT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    FOREIGN KEY(photo_id) REFERENCES Photos(id),
-    FOREIGN KEY(user_id) REFERENCES Users(id),
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
     PRIMARY KEY(user_id, photo_id)
 );
 
-CREATE TABLE Follows(
-    followers_id INT NOT NULL,
-    followed_id INT NOT NULL,
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (followers_id) REFERENCES Users(id),
-    FOREIGN KEY (followed_id) REFERENCES Users(id),
-    PRIMARY KEY (followed_id, followers_id)
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
 );
 
-CREATE TABLE Tags(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tag_name VARCHAR(255) UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW()
+CREATE TABLE tags (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE Photo_Tags(
-    photo_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    FOREIGN KEY (photo_id) REFERENCES Photos(id),
-    FOREIGN KEY (tag_id) REFERENCES Tags(id),
-    PRIMARY KEY (photo_id, tag_id)
+CREATE TABLE photo_tags (
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
 );
-
